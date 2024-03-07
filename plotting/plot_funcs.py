f057dd9ad25e30916c46aa03f2c843a4332fa236
Dan Carber <whasian63@gmail.com>
2024-03-07 14:47:00 -0600

    Bug fix

--- plotting/plot_funcs.py
+++ plotting/plot_funcs.py
@@ -227,7 +227,7 @@ def plot_histogram_2d(rf, desc):
     contents, xedges, yedges = load_histograms(rf, desc.vars)
     x, y = np.meshgrid(xedges, yedges)
     pc = ax.pcolormesh(x, y, contents.transpose(), cmap='Blues')
-    fig.colorbar(pc, ax=ax)
+    figure.colorbar(pc, ax=ax)
     ax.set_xlabel(desc.xlabel)
     ax.set_ylabel(desc.ylabel)
     figure.suptitle(desc.title)
