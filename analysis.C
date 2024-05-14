45900aee8c68ea0930ef253c2fac03b97ffcc78d
Dan Carber <whasian63@gmail.com>
2024-05-14 11:50:51 -0500

    POT scaling

--- analysis.C
+++ analysis.C
@@ -61,19 +61,19 @@ void analysis()
     spectra.add_spectrum1d("sCountPrimaries", Binning::Simple(20, 0, 20), kCountPrimaries);
     spectra.add_spectrum1d("sCountParticlesTruth", Binning::Simple(20, 0, 20), kCountParticlesTruth);
     spectra.add_spectrum1d("sCountPrimariesTruth", Binning::Simple(20, 0, 20), kCountPrimariesTruth);
-    spectra.add_spectrum1d("sEnergy_1e1p_signal_bias", Binning::Simple(50,-1,1), kEnergy_1e1p_signal_bias);
-    spectra.add_spectrum1d("sEnergy_1e1p_othernu_bias", Binning::Simple(50,-1,1), kEnergy_1e1p_othernu_bias);
-    spectra.add_spectrum1d("sEnergy_1e1p_cosmic_bias", Binning::Simple(50,-1,1), kEnergy_1e1p_cosmic_bias);
-    spectra.add_spectrum1d("sEnergy_1eNp_1p_signal_bias", Binning::Simple(50,-1,1), kEnergy_1eNp_1p_signal_bias);
-    spectra.add_spectrum1d("sEnergy_1eNp_Np_signal_bias", Binning::Simple(50,-1,1), kEnergy_1eNp_Np_signal_bias);
-    spectra.add_spectrum1d("sEnergy_1eNp_othernu_bias", Binning::Simple(50,-1,1), kEnergy_1eNp_othernu_bias);
-    spectra.add_spectrum1d("sEnergy_1eNp_cosmic_bias", Binning::Simple(50,-1,1), kEnergy_1eNp_cosmic_bias);
+    spectra.add_spectrum1d("sEnergy_1e1p_signal_bias", Binning::Simple(50,-1,2.4e20), kEnergy_1e1p_signal_bias);
+    spectra.add_spectrum1d("sEnergy_1e1p_othernu_bias", Binning::Simple(50,-1,2.4e20), kEnergy_1e1p_othernu_bias);
+    spectra.add_spectrum1d("sEnergy_1e1p_cosmic_bias", Binning::Simple(50,-1,2.4e20), kEnergy_1e1p_cosmic_bias);
+    spectra.add_spectrum1d("sEnergy_1eNp_1p_signal_bias", Binning::Simple(50,-1,2.4e20), kEnergy_1eNp_1p_signal_bias);
+    spectra.add_spectrum1d("sEnergy_1eNp_Np_signal_bias", Binning::Simple(50,-1,2.4e20), kEnergy_1eNp_Np_signal_bias);
+    spectra.add_spectrum1d("sEnergy_1eNp_othernu_bias", Binning::Simple(50,-1,2.4e20), kEnergy_1eNp_othernu_bias);
+    spectra.add_spectrum1d("sEnergy_1eNp_cosmic_bias", Binning::Simple(50,-1,2.4e20), kEnergy_1eNp_cosmic_bias);
 
-    spectra.add_spectrum1d("sNuEnergy_1e1p_signal_bias", Binning::Simple(50,-1,1), kNuEnergy_1e1p_signal_bias);
-    spectra.add_spectrum1d("sNuEnergy_1e1p_othernu_bias", Binning::Simple(50,-1,1), kNuEnergy_1e1p_othernu_bias);
-    spectra.add_spectrum1d("sNuEnergy_1eNp_1p_signal_bias", Binning::Simple(50,-1,1), kNuEnergy_1eNp_1p_signal_bias);
-    spectra.add_spectrum1d("sNuEnergy_1eNp_Np_signal_bias", Binning::Simple(50,-1,1), kNuEnergy_1eNp_Np_signal_bias);
-    spectra.add_spectrum1d("sNuEnergy_1eNp_othernu_bias", Binning::Simple(50,-1,1), kNuEnergy_1eNp_othernu_bias);
+    spectra.add_spectrum1d("sNuEnergy_1e1p_signal_bias", Binning::Simple(50,-1,2.4e20), kNuEnergy_1e1p_signal_bias);
+    spectra.add_spectrum1d("sNuEnergy_1e1p_othernu_bias", Binning::Simple(50,-1,2.4e20), kNuEnergy_1e1p_othernu_bias);
+    spectra.add_spectrum1d("sNuEnergy_1eNp_1p_signal_bias", Binning::Simple(50,-1,2.4e20), kNuEnergy_1eNp_1p_signal_bias);
+    spectra.add_spectrum1d("sNuEnergy_1eNp_Np_signal_bias", Binning::Simple(50,-1,2.4e20), kNuEnergy_1eNp_Np_signal_bias);
+    spectra.add_spectrum1d("sNuEnergy_1eNp_othernu_bias", Binning::Simple(50,-1,2.4e20), kNuEnergy_1eNp_othernu_bias);
 
     /**
      * Spectra (2D) for counting selection statistics by interaction categorization (efficiency).
